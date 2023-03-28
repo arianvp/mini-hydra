721ac1ff4062d85f34cafa936d2e179bf8b718d1
Arian van Putten <arian@mercury.com>
2023-03-28 18:14:51 +0200

    add some packages

--- flake.nix
+++ flake.nix
@@ -2,6 +2,9 @@
   description = "A very basic flake";
   inputs.nix-eval-jobs.url = "github:nix-community/nix-eval-jobs";
   outputs = { self, nixpkgs, nix-eval-jobs }: {
+    packages.x86_64-linux = {
+      inherit (nixpkgs.legacyPackages.x86_64-linux) hello cowsay fortune;
+    };
     packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
     packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
   };
